d78881cf1a065b578c8107292bdf62c7f428f55d
Arthur Flachs <arthur.flachs@gmail.com>
2021-01-15 15:27:52 +0200

    Add empty editor

--- components/color/color-editor/index.tsx
+++ components/color/color-editor/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import { Editor, EditorState } from 'draft-js';
 import styles from './styles.module.css';
 
 export default function ColorEditor() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  
   return (
     <div className={styles.editor}>
-      Color Editor
+      <Editor editorState={editorState} onChange={setEditorState} />
     </div>
   );
 }
