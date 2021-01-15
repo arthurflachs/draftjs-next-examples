d44fcd0618007935eb51c4d674e9ea6a99c70819
Arthur Flachs <arthur.flachs@gmail.com>
2021-01-15 20:51:11 +0200

    Import HTML without decorators

--- pages/examples/convert-from-html/index.tsx
+++ pages/examples/convert-from-html/index.tsx
@@ -1,18 +1,35 @@
 import React, { MouseEvent, useState } from "react";
-import { convertToRaw, EditorState } from "draft-js";
+import { ContentState, convertFromHTML, convertToRaw, EditorState } from "draft-js";
 import Layout from "../../../components/layout";
 import Editor from "../../../components/editor";
 import styles from './styles.module.css';
 
-export default function ConvertFromHTMLExample() {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+const sampleMarkup =
+  '<b>Bold text</b>, <i>Italic text</i><br/ ><br />' +
+  '<a href="http://www.facebook.com">Example link</a><br /><br/ >' +
+  '<img src="/convert-from-html/image.png" height="112" width="200" />';
 
+export default function ConvertFromHTMLExample() {
+  // TODO: support server side ?
+  if (typeof document === 'undefined') {
+    return null;
+  }
+  
   const logState = (event: MouseEvent) => {
     event.preventDefault();
     const content = editorState.getCurrentContent();
     console.log(convertToRaw(content));
   }
-  
+
+  const blocksFromHTML = convertFromHTML(sampleMarkup);
+  const contentState = ContentState.createFromBlockArray(
+    blocksFromHTML.contentBlocks,
+    blocksFromHTML.entityMap,
+  );
+  const [editorState, setEditorState] = useState(
+    EditorState.createWithContent(contentState)
+  );
+
   return (
     <Layout>
       <Editor
