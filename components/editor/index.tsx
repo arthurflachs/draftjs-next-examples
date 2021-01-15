1a0346304ad09556c2a8fb80284cae0a2b3ba900
Arthur Flachs <arthur.flachs@gmail.com>
2021-01-15 20:37:37 +0200

    Add focus on click

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,4 +1,5 @@
 import { DraftStyleMap, Editor, EditorState } from "draft-js";
+import { useRef } from "react";
 import NoSSR from "react-no-ssr";
 import styles from './styles.module.css';
 
@@ -8,10 +9,12 @@ interface EditorProps {
   customStyleMap?: DraftStyleMap;
 }
 export default function CustomEditor(props: EditorProps) {
+  const editorRef = useRef(null);
+
   return (
-    <div className={styles.editor}>
+    <div className={styles.editor} onClick={() => editorRef.current.focus()}>
       <NoSSR>
-        <Editor {...props} />
+        <Editor {...props} ref={editorRef} />
       </NoSSR>
     </div>
   );
