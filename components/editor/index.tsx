39708bbb9b3b968a30d6f95df6e1df211fdcec81
Arthur Flachs <arthur.flachs@gmail.com>
2021-01-16 10:46:46 +0200

    Refactor: use EditorProps from Draft JS

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,13 +1,8 @@
-import { DraftStyleMap, Editor, EditorState } from "draft-js";
+import { DraftStyleMap, Editor, EditorProps, EditorState } from "draft-js";
 import { useRef } from "react";
 import NoSSR from "react-no-ssr";
 import styles from './styles.module.css';
 
-interface EditorProps {
-  editorState: EditorState;
-  onChange(editorState: EditorState): any;
-  customStyleMap?: DraftStyleMap;
-}
 export default function CustomEditor(props: EditorProps) {
   const editorRef = useRef(null);
 
