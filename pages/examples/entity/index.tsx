10217ef1db8b01ede35d38b92b8968320860119f
Arthur Flachs <arthur.flachs@gmail.com>
2021-01-16 10:45:01 +0200

    Create Entity example page

---
+++ pages/examples/entity/index.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { EditorState } from "draft-js";
+import Editor from "../../../components/editor";
+
+export default function EntityExample() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  return (
+    <Editor
+      editorState={editorState}
+      onChange={setEditorState}
+    />
+  );
+}
